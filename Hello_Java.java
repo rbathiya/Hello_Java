dd7d1528631b168d387cea1e4a92569495ea447e
Rakhi Vijay Bathiya <rbathiya@tibco.com>
2023-03-13 13:43:38 +0530

    modifing

--- Hello_Java.java
+++ Hello_Java.java
@@ -1,7 +1,7 @@
 public class Hello_Java{
 	public static void main(String[] args){
 		System.out.println("Wake uo to Reality!");
-		
+		System.out.println("Nothing goes as plan in this world");
 	}
 	
 }
